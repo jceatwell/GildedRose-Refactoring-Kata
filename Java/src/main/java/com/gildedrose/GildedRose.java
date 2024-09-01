c521a54a393651c266d6e1c7035a70beb74719b2
jceatwell <jceatwell@gmail.com>
2024-09-01 17:18:28 +0200

    Moved all methods to new InventoryItem class
    Procedure:
    1. Start at the Top of GildedRoseClass and add an "InventoryItem" instance to each method.
    2. Let the IDE "fix" the method signature by adding in new parameters to match calling signature
    3. Run Unit tests after each update. Note that the InventoryItem object is essentually useless though makes migrating to Template Method easier.
    4. Once complete start at the bottom of the GildedRose class and start moving the methods to the Template Method Class (InventoryItem), using Refactor > Move Instance Method ...
    5. Choose "Protected" visibility
    6. Run Unit Tests after each move

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -10,66 +10,8 @@ class GildedRose {
 
     public void updateInventory() {
         for (Item item : items) {
-            InventoryItem inventoryItem = new InventoryItem(item);
-            updateItem(item);
+            new InventoryItem(item).dailyUpdate(item);
         }
     }
 
-    private void updateItem(Item item) {
-        updateQuality(item);
-        updateExpiration(item);
-        if (isExpired(item)) {
-            processExpired(item);
-        }
-    }
-
-    private void updateQuality(Item item) {
-        if (item.name.equals(InventoryItem.AGED_BRIE)) {
-            increaseQuality(item);
-        } else if (item.name.equals(InventoryItem.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-            increaseQuality(item);
-            if (item.sellIn < 11) {
-                increaseQuality(item);
-            }
-
-            if (item.sellIn < 6) {
-                increaseQuality(item);
-            }
-        } else if (!item.name.equals(InventoryItem.SULFURAS_HAND_OF_RAGNAROS)) {
-            decreaseQuality(item);
-        }
-    }
-
-    private void updateExpiration(Item item) {
-        if (item.name.equals(InventoryItem.SULFURAS_HAND_OF_RAGNAROS)) {
-            return;
-        }
-        item.sellIn = item.sellIn - 1;
-    }
-
-    private boolean isExpired(Item item) {
-        return item.sellIn < 0;
-    }
-
-    private void processExpired(Item item) {
-        if (item.name.equals(InventoryItem.AGED_BRIE)) {
-            increaseQuality(item);
-        } else if (item.name.equals(InventoryItem.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-            item.quality = 0;
-        } else if (!item.name.equals(InventoryItem.SULFURAS_HAND_OF_RAGNAROS)) {
-            decreaseQuality(item);
-        }
-    }
-
-    private void increaseQuality(Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-        }
-    }
-
-    private void decreaseQuality(Item item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
-        }
-    }
 }
--- Java/src/main/java/com/gildedrose/InventoryItem.java
+++ Java/src/main/java/com/gildedrose/InventoryItem.java
@@ -12,4 +12,61 @@ public class InventoryItem {
         this.item = item;
     }
 
+    protected void processExpired(Item item) {
+        if (item.name.equals(AGED_BRIE)) {
+            increaseQuality(item);
+        } else if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+            item.quality = 0;
+        } else if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+            decreaseQuality(item);
+        }
+    }
+
+    protected boolean isExpired(Item item) {
+        return item.sellIn < 0;
+    }
+
+    protected void updateQuality(Item item) {
+        if (item.name.equals(AGED_BRIE)) {
+            increaseQuality(item);
+        } else if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+            increaseQuality(item);
+            if (item.sellIn < 11) {
+                increaseQuality(item);
+            }
+
+            if (item.sellIn < 6) {
+                increaseQuality(item);
+            }
+        } else if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+            decreaseQuality(item);
+        }
+    }
+
+    public void dailyUpdate(Item item) {
+        updateQuality(item);
+        updateExpiration(item);
+        if (isExpired(item)) {
+            processExpired(item);
+        }
+    }
+
+    protected void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality --;
+        }
+    }
+
+    protected void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+
+    protected void updateExpiration(Item item) {
+        if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+            return;
+        }
+        item.sellIn--;
+    }
 }
