adc072f3d85cb50346a6fbe4bd2004bcf88267b1
jceatwell <jceatwell@gmail.com>
2024-09-01 15:03:57 +0200

    rearrange main class to make more radable

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -11,6 +11,12 @@ class GildedRose {
         this.items = items;
     }
 
+    public void updateInventory() {
+        for (Item item : items) {
+            updateItem(item);
+        }
+    }
+
     private static void updateItem(Item item) {
         updateQuality(item);
         updateExpiration(item);
@@ -57,21 +63,15 @@ class GildedRose {
         }
     }
 
-    private static void decreaseQuality(Item item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
-        }
-    }
-
     private static void increaseQuality(Item item) {
         if (item.quality < 50) {
             item.quality = item.quality + 1;
         }
     }
 
-    public void updateInventory() {
-        for (Item item : items) {
-            updateItem(item);
+    private static void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
         }
     }
 }
